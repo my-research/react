8cb6d8fefca36c77d45219a758e89df15ee18f2e
dhslrl321 <dhslrl321@gmail.com>
2022-04-25 09:50:35 +0900

    commit

--- src/main/java/com/example/springsecurityforjwt/SpringSecurityForJwtApplication.java
+++ src/main/java/com/example/springsecurityforjwt/SpringSecurityForJwtApplication.java
@@ -2,9 +2,22 @@ package com.example.springsecurityforjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityForJwtApplication {
+
+    @RestController
+    public static class HelloController {
+        @GetMapping("/")
+        public String hello() {
+            WebSecurityConfiguration
+            return "Hello World";
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityForJwtApplication.class, args);
