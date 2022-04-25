7199023b8ff2b6596366f274bb2f10c4056f300b
dhslrl321 <dhslrl321@gmail.com>
2022-04-26 00:52:54 +0900

    commit

--- src/test/java/com/example/springsecurityforjwt/ApplicationTests.java
+++ src/test/java/com/example/springsecurityforjwt/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringSecurityForJwtApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
